7bc933accde7b741e5fd1a14a62aa6ef7627d89e
Calista Annelise <calista.annelise@gmail.com>
2026-09-24 07:22:09 -0700

    Add: find-duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int i = 0;
+        while (true) {
+            if (nums[i] == -1) {
+                return i;
+            }
+            int temp = nums[i];
+            nums[i] = -1;
+            i = temp;
+        }
+    }
+}
