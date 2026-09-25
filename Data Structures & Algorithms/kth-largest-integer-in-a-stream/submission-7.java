46773bf84b0ef7f06376deb6dd22b9eb9e53fdde
Calista Annelise <calista.annelise@gmail.com>
2026-09-25 08:12:48 -0700

    Add: kth-largest-integer-in-a-stream - submission-7

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-7.java
@@ -0,0 +1,26 @@
+class KthLargest {
+    PriorityQueue<Integer> pq;
+    int k;
+    int size;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.size = 0;
+        this.pq = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            pq.add(nums[i]);
+            size++;
+            if (size > k) {
+                pq.poll();
+            }
+        }
+    }
+    
+    public int add(int val) {
+        pq.add(val);
+        size++;
+        if (size > k) {
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
